2d2a45f263066f65e694d715a33414ff4f5e2618
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-03-05 15:07:48 +0400

    newfeature

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,14 +11,14 @@ import {
   UnauthorizedException,
   NotFoundException,
   UseGuards,
+  BadRequestException,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import * as jwt from 'jsonwebtoken'; // Import JWT module
+import * as jwt from 'jsonwebtoken'; 
 import { ConfigService } from '@nestjs/config';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { SellerGuard } from 'src/auth/guards/seller.guard';
-
 
 @Controller('products')
 export class ProductsController {
@@ -50,15 +50,29 @@ export class ProductsController {
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
-
+  @UseGuards(SellerGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
-  
-  
+
+  @UseGuards(SellerGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(id);
+  async remove(
+      @Param('id') id: string,
+      @Headers('authorization') authorization: string,
+  ) {
+      if (!authorization || !authorization.startsWith('Bearer ')) {
+          throw new BadRequestException('Authorization header missing or INVALID');
+      }
+  
+      const token = authorization.split(' ')[1];
+      const decoded = jwt.verify(
+          token,
+          this.configService.get<string>('JWT_SECRET'),
+      ) as { userId: string }; 
+  
+      return this.productsService.remove(id, decoded.userId);
   }
 }
+  
