d9b18916420bbda7478834e9dffe09dee322e321
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-03-19 23:11:07 +0400

    feature/adding adinjs + moonguse

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,6 @@ import { EmailModule } from './email/email.module';
 import { AwsS3Module } from './upload/aws-s3.module';
 import { Product, ProductSchema } from './products/schema/product.schema';
 
-export const dynamicImport = async (packageName: string) =>
-  new Function(`return import('${packageName}')`)();
-
 const DEFAULT_ADMIN = {
   email: 'admin@example.com',
   password: 'password',
@@ -28,15 +25,36 @@ const authenticate = async (email: string, password: string) => {
   return null;
 };
 
-
+const dynamicImport = async (packageName: string) => {
+  try {
+    const module = await import(packageName);
+    return module;
+  } catch (error) {
+    console.error(`Error importing package: ${packageName}`, error);
+    throw error;
+  }
+};
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    import('@adminjs/nestjs').then(({ AdminModule }) =>
-      import('adminjs').then()=>
-      AdminModule.createAdminAsync({
+    dynamicImport('@adminjs/nestjs').then(async ({ AdminModule }) => {
+      // Dynamically import the necessary modules
+      const { default: AdminJS } = await dynamicImport('adminjs');
+      const { default: mongoose } = await dynamicImport('mongoose');
+      const { default: mongooseAdapter } =
+        await dynamicImport('@adminjs/mongoose');
+
+      const mongooseDb = await mongoose.connect(
+        'mongodb://localhost:27017/test',
+      );
+
+      const admin = new AdminJS({
+        databases: [mongooseAdapter(mongooseDb)],
+      });
+
+      return AdminModule.createAdminAsync({
         useFactory: () => ({
           adminJsOptions: {
             rootPath: '/admin',
@@ -53,8 +71,8 @@ const authenticate = async (email: string, password: string) => {
             secret: 'secret',
           },
         }),
-      }),
-    ),
+      });
+    }),
     UsersModule,
     AuthModule,
     ProductsModule,
